bfd671d1dfc2e8d548f56bc3038dfa8d8262d576
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2018-07-10 17:49:08 +0100

    guizero exampe

---
+++ examples/guizero_display.py
@@ -0,0 +1,16 @@
+from guizero import App, Text, Combo, Picture
+from quickdraw import QuickDrawData
+
+def display_drawing():
+    display.image = qd.get_drawing(drawing_name.value).image
+
+qd = QuickDrawData()
+
+app = App(title="Quick, Draw! Viewer")
+Text(app, text="Pick a drawing")
+drawing_name = Combo(app, options=qd.drawing_names, selected=qd.drawing_names[0], command=display_drawing)
+display = Picture(app)
+status = Text(app)
+display_drawing()
+display.repeat(1000, display_drawing)
+app.display()
